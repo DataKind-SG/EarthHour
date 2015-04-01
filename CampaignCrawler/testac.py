beff9e9781d986293dbe56fec2c978c654b96d49
Yoke Keong Wong <wongyokekeong@gmail.com>
2015-04-02 00:41:02 +0800

    added pagination support for campaign paginator

--- CampaignCrawler/testac.py
+++ CampaignCrawler/testac.py
@@ -2,12 +2,42 @@ from includes.ActiveCampaign import ActiveCampaign
 from includes.Config import ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY
 import datetime, time
 
+def test_paginator():
+    print 'Testing campaign paginator ...'
+    ac = ActiveCampaign(ACTIVECAMPAIGN_URL,  ACTIVECAMPAIGN_API_KEY)
+    ## 1. tested that hard limit of campaign paginator is 100 campaigns
+    ## 2. offset skips campaigns
+    ## 3. campaigns['total'] will tell you total number of campaigns available
+    ## 4. campaigns['cnt'] returns current count of campaigns returned
+    campaigns = ac.api('campaign/paginator?sort=&offset=0&limit=100&filter=0&public=0')
+    campaign_rows = campaigns['rows']
+    print "rows recorded:" + str(len(campaign_rows))
+    if campaigns['total'] > 100:
+        outstanding_campaigns = campaigns['total'] - 100
+        offset = 100
+        print "More than 100 rows detected"
+        print "outstanding_campaigns:" + str(outstanding_campaigns)
+        print "offset:" + str(offset)
+        while outstanding_campaigns > 0:
+            campaigns = ac.api('campaign/paginator?sort=&offset='+str(offset)+'&limit=100&filter=0&public=0')
+            campaign_rows.extend(campaigns['rows'])
+            outstanding_campaigns = outstanding_campaigns - 100
+            offset = offset + 100
+            print "outstanding_campaigns in loop:" + str(outstanding_campaigns)
+            print "offset in loop:" + str(offset)
+            print "rows recorded:" + str(len(campaign_rows))
+        
+
 if __name__ == '__main__':
     ac = ActiveCampaign(ACTIVECAMPAIGN_URL,  ACTIVECAMPAIGN_API_KEY)
     
     campaigns = ac.api('campaign/paginator?sort=&offset=0&limit=20&filter=0&public=0')
+    offset = 0
+    campaigns = ac.api('campaign/paginator?sort=&offset='+str(offset)+'&limit=20&filter=0&public=0')
     crows = campaigns['rows']
     
+    # documentation points to limit of 20 items per page
+    campaign5open = ac.api('campaign/report_open_list?campaignid=5&page=2')
     print ac.api('campaign/report_open_list?campaignid=5&page=1')
     
     campaign11fwd = ac.api('campaign/report_forward_list?campaignid=11&messageid=16')
@@ -19,7 +49,7 @@ if __name__ == '__main__':
     
     campaign5opentotal = ac.api('campaign/report_open_totals?campaignid=5')
 
-    ac = ActiveCampaign(ACTIVECAMPAIGN_URL,  ACTIVECAMPAIGN_API_KEY)
+    #ac = ActiveCampaign(ACTIVECAMPAIGN_URL,  ACTIVECAMPAIGN_API_KEY)
 
     ## create campaign code
 #    import datetime, time
@@ -37,10 +67,9 @@ if __name__ == '__main__':
 #            'p[1]': 1, # use list 1 - "gregs"
 #            'm[10]': 100 # use message id 10, send to 100% of list
 #        }
-#        from time import time
-#        time2 = time()
+#        time2 = time.time()
 #        print ac.api('campaign/create', campaign)
-#        print 'diff2 = %.5f seconds' %(time() - time2)
+#        print 'diff2 = %.5f seconds' %(time.time() - time2)
     
     #print ac.api('message/list?ids=10')
 
